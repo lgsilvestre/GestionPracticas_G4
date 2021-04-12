cddb1fb5d324a9d3c68ce954969fb3ec2e79de4e
filip-el-lagarto <felipe.fuenzalid@gmail.com>
2021-04-12 10:30:42 -0400

    Integración front
    React app

---
+++ react-practicas-front/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import View from './components/view-example.js';
+
+
+function App() {
+  return (
+    <div className="App">
+      <View />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-practicas-front/src/components/view-example.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Button } from 'reactstrap';
+
+class View extends React.Component {
+
+    render() {
+        return (
+            <Button type="button" class="btn btn-dark">App practicas</Button>
+        );
+    }
+}
+
+export default View;
