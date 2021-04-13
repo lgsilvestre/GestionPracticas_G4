fb31efd748fa4cbe521edd9423b48d2ec08d82a0
Leo Miranda <leomir2656gmail.com>
2021-04-13 18:01:33 -0400

    SideBar
    Menu lateral vista administrativa

---
+++ react-practicas-front/src/components/Pages/Estudiante.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Estudiante = () => {
+    return (
+        <div>
+            <h1>Esudiantes</h1>
+        </div>
+    )
+}
+
+export default Estudiante
--- react-practicas-front/src/components/VistaAdmin.js
+++ react-practicas-front/src/components/VistaAdmin.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Ofertas from './Pages/Ofertas';
+import Postulaciones from './Pages/Postulaciones';
+import Estudiante from './Pages/Estudiante';
+import Practicas from './Pages/Practicas';
+import SideBar from './SideBar/SideBar'
 
 const VistaAdmin = () => {
     return (
         <div>
-             <h1>Vista Admin</h1>
+             <Router>
+                <SideBar />
+                <Switch>
+                    <Route path='/practicas' exact component={Practicas} />
+                    <Route path='/estudiantes' component={Estudiante} />
+                    <Route path='/postulaciones' component={Postulaciones} />
+                    <Route path='/ofertas' component={Ofertas} />
+                </Switch>
+            </Router>
         </div>
     )
 }
