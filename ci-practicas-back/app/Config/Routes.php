13627297f7e55ffe005dff017ef80bccdcd73af6
krnl <felipe.fuenzalid@gmail.com>
2021-04-25 19:14:30 -0400

    Gestión de usuarios
    25-04-2021

--- ci-practicas-back/app/Config/Routes.php
+++ ci-practicas-back/app/Config/Routes.php
@@ -47,6 +47,12 @@ $routes->get('/', 'Home::index');
  * You will have access to the $routes object within that file without
  * needing to reload it.
  */
+<<<<<<< Updated upstream
+=======
+$routes->match(['get','post'],'login', 'UsersController::login',['filter' => 'NoAuth']);
+$routes->match(['get','post'],'profile', 'UsersController::profile');
+
+>>>>>>> Stashed changes
 if (file_exists(APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php'))
 {
 	require APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php';
---
+++ ci-practicas-back/app/Controllers/UserController.php
@@ -0,0 +1,106 @@
+<?php namespace App\Controllers;
+
+use App\Models\UserModel;
+class Users extends BaseController
+{
+    public function login(){
+        helper(['form']);
+        if($this-> request -> getMethod() == 'post') {
+            $rules = [
+                'email' => 'required|min_length[6]|max_length[99]|valid_email',
+                'password' => 'required|max_length[255]|validateUSer[email, password]',
+            ];
+            $errors = [
+                'password' => [
+                'validateUSer' => 'Email y contraseña no coinciden'
+                ]
+            ];
+
+            if(! $this->validate($rules, $errors)){
+                $data['validation'] = $this->validator;
+            } else{
+                $model = new UserModel();
+                $user = $model->where('email', $this->request->getVar('email'))
+                             ->first();
+
+                $this-> setUserSession($user); // aqui tenemos ya al usuario que corresponde
+
+                if($user['tipo']==0){//Superadmin
+                    //return redirect()->to('/dashbordSuperAdmin');
+                }
+                if($user['tipo']==1){//Admin
+                    //return redirect()->to('/dashbordAdmin');
+                }
+                if($user['tipo']==2){//cliente
+                    //return redirect()->to('/dashbordJefeCarrera');
+                }
+                if($user['tipo']==3){//cliente
+                    //return redirect()->to('/dashbordEncarcadoCarrera');
+                }
+                if($user['tipo']==4){//cliente
+                    //return redirect()->to('/dashbordAlumno');
+                }
+            }
+        }
+
+        //echo view('template/header',$data);
+        //echo view('template/landing/login');
+        //echo view('template/footer');
+    }
+
+    private function setUserSession($user){
+        $data =[
+            'id' => $user['id'],
+            'nombre' => $user['firstname'],
+            'email' => $user['email'],
+            'tipo' => $user['tipo'],
+            'permisos' => $user['permisos'],
+            'isLoggedIn' => true,
+        ];
+        session()->set($data);
+        return true;
+    }
+
+    public function profile(){
+        $model = new UserModel();
+        helper(['form');
+
+        if($this-> request -> getMethod() == 'post') {
+            //validation rules
+            $rules = [
+                'nombre' => 'required|min_length[3]|max_length[22]',
+               // 'email' => 'required|min_length[3]|max_length[22]',
+                'password' =>'required',
+               // 'permisos' =>'required'
+            ];
+            if($this->request->getPost('clave') != ''){
+                $rules['clave'] = 'required|min_length[6]|max_length[255]';
+                $rules['confirmar'] =  'matches[clave]';
+            }
+            $errors = [
+                'email' => [
+                    'is_unique' => 'Email se encuentra registrado en el sistema'
+                ]
+            ];
+
+            if(! $this->validate($rules, $errors)){
+                $data['validation'] = $this->validator;
+            } else{
+                $newsData =[
+                    'id' => session()->get('id'),
+                    'firstname' => $this->request->getPost('nombre'),
+                ];
+                if($this->request->getPost('clave') != ''){
+                    $newsData['password'] = $this->request->getPost('clave');
+                }
+                $model -> save($newsData);
+
+                //session()->setFlashdata('success','Informacion de usuario editada con exito');
+
+              //  return redirect()->to('/profile');
+            }
+        }
+    //redirigir por ruta con los datos en json
+    }
+    
+}
