0d2d9ce057a9cd0466eaa1699adb989385115191
Camilo Villalobos <camilo.svs@gmail.com>
2021-04-26 12:24:36 -0400

    Configuracion de stilo para el wizard y nuevas librerias *npm install requerido*

--- react-practicas-front/src/components/Pages/EstadosPractica/FormPostulacion.js
+++ react-practicas-front/src/components/Pages/EstadosPractica/FormPostulacion.js
@@ -1,20 +1,30 @@
 import React from 'react'
 import {
-    Button
+    Button, Col, Card, CardBody
   } from 'reactstrap';
 
 export const FormPostulacion = ({handleSubmit}) => {
 
     return (
-        <div>
+        <div className="container">
             <h4>Formularion de Postulaciuon</h4>
             <hr/>
             <form onSubmit={handleSubmit}>
-                <div style={{ paddingBottom: 30 }}>
-                    <Button color="primary" className="btn-pill pull-right" type="submit" style={{marginRight: '20px'}}>
-                        Next
-                    </Button>
-                </div>
+                <Col>
+                    <Card>
+                        <CardBody>
+                            
+                        </CardBody>
+                        <div style={{ paddingBottom: 30 }}>
+                            <Button color="primary" className="btn-pill pull-right" type="submit" style={{marginRight: '20px'}}>
+                                Next &nbsp;
+                                <i className="fa fa-chevron-right" />
+                            </Button>
+                        </div>
+                    </Card>
+                </Col>
+
+                
             </form>
             
         </div>
