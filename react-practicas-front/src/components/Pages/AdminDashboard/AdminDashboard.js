13d0b137a4bee8e58470a28c99ab470b8dbd539d
Leo Miranda <leomir2656gmail.com>
2021-06-14 17:07:37 -0400

    Cards Dashboard Admin

--- react-practicas-front/src/components/Pages/AdminDashboard/AdminDashboard.js
+++ react-practicas-front/src/components/Pages/AdminDashboard/AdminDashboard.js
@@ -1,18 +1,25 @@
-import React, { Fragment, useState } from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
+import { IconContext } from "react-icons";
+import { BiNetworkChart } from "react-icons/bi";
+import { CgFileDocument } from "react-icons/cg";
+import { BiTask } from "react-icons/bi";
+import Divider from '@material-ui/core/Divider';
 
 const useStyles = makeStyles({
     root: {
       maxWidth: 345,
+      borderRadius: '25px',
+      height: '78%'
+   
     },
     media: {
       height: 140,
@@ -23,8 +30,53 @@ const useStyles = makeStyles({
       },
       titulo:{
        fontSize: '4em',
-       color: '#1A2B4B'
+       color: '#1A2B4B',
+       alignSelf: 'right'
       },
+      icono:{
+      flex:' 0 0 43.333333%',
+      maxWidth: '43.333333%',
+      alignSelf: 'left'
+      },
+      label: {
+        boxSizing: 'border-box',
+        display: 'block',
+      
+        marginRight:'1em',
+        textAlign: 'right',
+        color: '#9A9A9A',
+        fontSize: '23px',
+        lineHeight: '1.4em'
+      },
+      cardContent: {
+        display: 'flex',
+        justifyContent: 'space-between'
+      },
+      cantidad: {
+        textAlign: 'right',
+        fontSize: '2em',
+        marginRight: '2em',
+        marginTop: '-2em',
+        fontFamily: '"Montserrat", "Helvetica Neue", Arial, sans-serif'
+      },
+      area : {
+          padding: '0'
+      },
+      boton: {
+        backgroundColor: '#fff',
+        color: '#f1A2B4Bff',  
+        cursor: 'pointer',
+        transition: 'all 0.4s cubic-bezier(0.42, 0, 0.58, 1)',
+        '&:hover': {
+        backgroundColor: '#f0ebcc',
+          color: '#000'
+        }
+      }, 
+      areaBoton: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }
   });
 
 export const AdminDashboard = () => {
@@ -45,57 +97,79 @@ export const AdminDashboard = () => {
 
                     <Grid item sm={4}>
                         <Card className={classes.root} style={{height:'100%'}}>
-                            <CardActionArea>
-                                <CardContent>
-                                    <Typography gutterBottom variant="h5" component="h2">
-                                        Prácticas en proceso: 16
-                                    </Typography>
-                                    <Typography variant="body2" color="textSecondary" component="p">
-                                        Revisa las prácticas que están siendo rendidas.
-                                    </Typography>
+                            <CardActionArea className={classes.area}>
+                                <CardContent className={classes.cardContent}>
+                                <IconContext.Provider value={{ size: "5em" , color: "#5E8B7E"}} className={classes.icono}>
+        
+                                    <BiNetworkChart  />
+                                    <Typography gutterBottom variant="h5" component="h2" className={classes.label}>
+                                        Proceso
+                                    </Typography>                                   
+                                    
+                                </IconContext.Provider>                                    
+                                        
                                 </CardContent>
+                                <Typography gutterBottom variant="h5" component="h2" className={classes.cantidad} >
+                                        16
+                                    </Typography> 
                             </CardActionArea>
-                            <CardActions>
-                                <Button size="small" color="primary">
+                            <Divider variant="middle" light={true} />
+                            <CardActions className={classes.areaBoton}>
+                                <Button className={classes.boton} size="small" color="primary">
                                     Ver prácticas
-                                </Button>
-                            </CardActions>
-                        </Card>
-                    </Grid>
-                    <Grid item sm={4}>
-                        <Card className={classes.root} style={{height:'100%'}}>
-                            <CardActionArea>
-                                <CardContent>
-                                    <Typography gutterBottom variant="h5" component="h2">
-                                        Solicitudes de práctica: 4
-                                    </Typography>
-                                    <Typography variant="body2" color="textSecondary" component="p">
-                                        Revisa las solicitudes de práctica pendientes.
-                                    </Typography>
-                                </CardContent>
-                            </CardActionArea>
-                            <CardActions>
-                                <Button size="small" color="primary">
-                                    Ver solicitudes
                                 </Button>
                             </CardActions>
                         </Card>
                     </Grid>
                     <Grid item sm={4}>
                     <Card className={classes.root} style={{height:'100%'}}>
-                            <CardActionArea>
-                                <CardContent>
-                                    <Typography gutterBottom variant="h5" component="h2">
-                                        Prácticas por terminar: 6
-                                    </Typography>
-                                    <Typography variant="body2" color="textSecondary" component="p">
-                                        Prácticas que terminarán en un período de 7 días o menos.
-                                    </Typography>
+                            <CardActionArea className={classes.area}>
+                                <CardContent className={classes.cardContent}>
+                                <IconContext.Provider value={{ size: "5em" , color: "#47597E"}} className={classes.icono}>
+        
+                                    <CgFileDocument  />
+                                    <Typography gutterBottom variant="h5" component="h2" className={classes.label}>
+                                        Solicitudes
+                                    </Typography>                                   
+                                    
+                                </IconContext.Provider>                                    
+                                        
                                 </CardContent>
+                                <Typography gutterBottom variant="h5" component="h2" className={classes.cantidad} >
+                                        34
+                                    </Typography> 
                             </CardActionArea>
-                            <CardActions>
-                                <Button size="small" color="primary">
-                                    Ver prácticas
+                            <Divider variant="middle" light={true} />
+                            <CardActions className={classes.areaBoton}>
+                                <Button className={classes.boton} size="small" color="primary">
+                                    Ver solicitudes
+                                </Button>
+                            </CardActions>
+                        </Card>
+                      
+                    </Grid>
+                    <Grid item sm={4}>
+                    <Card className={classes.root} style={{height:'100%'}}>
+                            <CardActionArea className={classes.area}>
+                                <CardContent className={classes.cardContent}>
+                                <IconContext.Provider value={{ size: "5em" , color: "#D8B384"}} className={classes.icono}>
+        
+                                    <BiTask  />
+                                    <Typography gutterBottom variant="h5" component="h2" className={classes.label}>
+                                        Finalizando
+                                    </Typography>                                   
+                                    
+                                </IconContext.Provider>                                    
+                                        
+                                </CardContent>
+                                <Typography gutterBottom variant="h5" component="h2" className={classes.cantidad} >
+                                        16
+                                    </Typography> 
+                            </CardActionArea>
+                            <Divider variant="middle" light={true} />
+                            <CardActions className={classes.areaBoton}>
+                                <Button className={classes.boton} size="small" color="primary">
+                                    Ver finalizando
                                 </Button>
                             </CardActions>
                         </Card>
