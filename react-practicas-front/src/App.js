cb5bfc535e7cccfd3ae28fa834897432840ee29d
Camilo Villalobos <camilo.svs@gmail.com>
2021-06-25 21:09:25 -0400

    pequeño bug

--- react-practicas-front/src/App.js
+++ react-practicas-front/src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { AppRouter } from './components/routers/AppRouter';
 import './components/routers/AppRouterStyle.css';
-import Modal from 'react-modal';
-
-Modal.setAppElement(document.getElementById('root'));
 
 function App() {
   return (
