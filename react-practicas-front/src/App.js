f6d75240f777f46f7e35b8120f3308099ae8f715
Camilo Villalobos <camilo.svs@gmail.com>
2021-06-25 20:45:51 -0400

    Boton "mas opciones" para info practica admin

--- react-practicas-front/src/App.js
+++ react-practicas-front/src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { AppRouter } from './components/routers/AppRouter';
 import './components/routers/AppRouterStyle.css';
-import { Testhook } from './hooks/Testhook';
+import Modal from 'react-modal';
+
+Modal.setAppElement(document.getElementById('root'));
 
 function App() {
   return (
